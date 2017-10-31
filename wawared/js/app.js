e761a2ec6dd0e36c768bdb01e6d8b4b7f087663a
efarfan2393 <e.farfanvaldivia@gmail.com>
2017-10-31 17:26:43 -0500

    add

--- wawared/js/app.js
+++ wawared/js/app.js
@@ -1,82 +1,48 @@
-
-
-var lastChildMenu = function(){
-	$("div #menuListGroup a:first-child").addClass("active");
-	$("div #menuListGroup a:last-child").removeClass("active");
-}
 var scrollDocument = function(){
-	$(window).scroll(function() { 
-		if ($(document).scrollTop() > 50) { 
-			$(".headerBlue").css("height", "80px");
-			$(".headerBlue").addClass("fixed-top")
-			$(".headerBlue").css("padding-top", "17px");
-			$(".imagen_logoMinsa").attr("src","img/logo_minsa_small.jpg");
-			$(".contentInfo").css("margin", "120px auto")
-			$(".tittleHeader").css("font-size", "20px")
-			$(".subTittleHeader").css("font-size", "15px")
-			$("#menuListGroup").addClass("positionFixed")
-			$(".contentWhite").addClass("mt-100")
-
- 		} else if ($(document).scrollTop() < 50) {
-		    $(".headerBlue").css("height", "220px");
-			$(".headerBlue").css("padding-top", "75px");
-			$(".imagen_logoMinsa").attr("src","img/logo_minsa_big.jpg"); 
-			$(".contentInfo").css("margin", "300px auto")
-			$(".tittleHeader").css("font-size", "2.5rem")
-			$(".subTittleHeader").css("font-size", "20px")
-			$("#menuListGroup").removeClass("positionFixed")
-			$(".contentWhite").removeClass("mt-100")
-		}
-		if ($(window).width() <= 770 ){
-			if ($(document).scrollTop() > 50) { 
-				$(".headerBlue").css("height", "80px");
-				$(".headerBlue").addClass("fixed-top")
-				$(".headerBlue").css("padding-top", "15px");
-				$(".imagen_logoMinsa").attr("src","img/logo_minsa_small.jpg");
-				$(".contentInfo").css("margin", "200px auto")
-				$(".tittleHeader").css("font-size", "20px")
-				$(".subTittleHeader").css("font-size", "15px")
-				$("#menuListGroup").removeClass("positionFixed")
-				$(".contentWhite").removeClass("mt-100")
-	 		} else if ($(document).scrollTop() < 50) {
-			   $(".headerBlue").css("height", "120px");
-				$(".headerBlue").addClass("fixed-top")
-				$(".headerBlue").css("padding-top", "35px");
-				$(".imagen_logoMinsa").attr("src","img/logo_minsa_small.jpg");
-				$(".contentInfo").css("margin", "150px auto")
-				$(".tittleHeader").css("font-size", "20px")
-				$(".subTittleHeader").css("font-size", "15px")
-				$("#menuListGroup").removeClass("positionFixed")
-				$(".contentWhite").removeClass("mt-100")
-			} 
-   		}
-	});
-}
-var responsivePhoneLogo = function(){
-	if ($(window).width() <= 770 ){
-		$(".imagen_logoMinsa").attr("src","img/logo_minsa_small.jpg");
-		$("#menuListGroup").removeClass("positionFixed");
-   	}
+    $(window).scroll(function() {
+        if($(window).width() > 992){
+            if ($(document).scrollTop() > 50) { 
+                $(".headerBlue").css("height", "80px");
+                $(".headerBlue").css("top", "0");
+                $(".headerBlue").css("padding-top", "17px");
+                $(".imagen_logoMinsa").attr("src","img/logo_minsa_small.jpg");
+                $(".contentInfo").css("padding-top", "120px")
+                $(".tittleHeader").css("font-size", "20px")
+                $(".subTittleHeader").css("font-size", "15px")
+                $("#menuListGroup").addClass("positionFixed")
+                $(".contentWhite").css("margin-top", "108px")
+            } else if ($(document).scrollTop() < 50) {
+                $(".headerBlue").css("height", "220px");
+                $(".headerBlue").css("padding-top", "75px");
+                $(".imagen_logoMinsa").attr("src","img/logo_minsa_big.jpg"); 
+                $(".contentInfo").css("padding-top", "280px")
+                $(".tittleHeader").css("font-size", "2.5rem")
+                $(".subTittleHeader").css("font-size", "20px")
+                $("#menuListGroup").removeClass("positionFixed")
+                $(".contentWhite").css("margin-top", "10px")
+            }
+        } 
+    });
 }
 var toggleFixed = function() {
    adjustWidth();
    $("#menuListGroup").toggleClass("positionFixed");
 }
 var adjustWidth = function(){
-	var parentwidth = $("#contentMenu").width();      
+    var parentwidth = $("#contentMenu").width();      
     $("#menuListGroup").toggleClass("positionFixed").width(parentwidth);
 }
 var resize = function(){
-	$(window).resize(
-	    function() {
-	    	adjustWidth();
-	})
+    $(window).resize(
+        function() {
+            adjustWidth();
+    })
 }
 $(document).ready(function(){
-	toggleFixed();
-	adjustWidth();
-	resize();
-	lastChildMenu();
-	responsivePhoneLogo();
+    $('.scrollspy').scrollSpy();
+    $(".button-collapse").sideNav();
     scrollDocument();
+    toggleFixed();
+    adjustWidth();
+    resize();
 })
